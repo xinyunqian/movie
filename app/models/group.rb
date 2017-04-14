3509de9219ccfc7d2c18ec8c13fb3773e0101402
钱多多 <qdd@qianduoduodeMacBook-Pro.local>
2017-04-14 19:05:54 +0800

    add user_id to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  validates :电影名称, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414094349) do
+ActiveRecord::Schema.define(version: 20170414104749) do
 
   create_table "groups", force: :cascade do |t|
     t.text     "电影名称"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170414094349) do
     t.integer  "上映年份"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|

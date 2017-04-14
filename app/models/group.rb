3228383bb963d494b84178c2674aa2a87b40d954
钱多多 <qdd@qianduoduodeMacBook-Pro.local>
2017-04-14 19:16:59 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
   validates :电影名称, presence: true
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end

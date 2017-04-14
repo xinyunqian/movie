d7b3da96112a28fa071965680a719980571b3ed2
钱多多 <qdd@qianduoduodeMacBook-Pro.local>
2017-04-14 19:29:22 +0800

    add post and hook group/user relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :posts
   validates :电影名称, presence: true
   has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414111553) do
+ActiveRecord::Schema.define(version: 20170414112802) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20170414111553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

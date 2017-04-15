983cccb2f9473cc561a71472a254fc48fd2df082
钱多多 <qdd@qianduoduodeMacBook-Pro.local>
2017-04-15 17:25:28 +0800

    DESC

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
 
   def index
     @groups = Group.all
@@ -11,6 +11,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts.recent
   end
 
   def edit
@@ -22,6 +23,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -46,6 +48,32 @@ class GroupsController < ApplicationController
 
     @group.destroy
     redirect_to groups_path, alert: '该影片已删除'
+  end
+
+  def join
+   @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "标记看过本片成功"
+    else
+      flash[:warning] = "你已标记看过本片了"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "取消标记看过本片成功！"
+    else
+      flash[:warning] = "您还没有标记看过本片哦，无法取消标记 XD"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
